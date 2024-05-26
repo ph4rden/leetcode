1e27c385b33d9ad98237d4de0d67caceb6116085
Brian <brianphan17@yahoo.com>
2024-05-26 09:02:48 +0700

    comments

--- arrays-hashing/group_anagrams.py
+++ arrays-hashing/group_anagrams.py
@@ -1,26 +1,23 @@
 from typing import List
-
-
 '''
-    
+    1. sort words
+    2. check if sorted word is in the dict
+    3. use sorted word as the key with the value being the word
+    4. return a list of the values from the dict
 '''
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
         for word in strs:
-            # Sort the word to form the key
             sorted_word = ''.join(sorted(word))
             
-            # If the key is not in the dictionary, create a new list
             if sorted_word not in anagrams:
                 anagrams[sorted_word] = []
                 
-            # Append the original word to the list corresponding to the sorted word
             anagrams[sorted_word].append(word)
         
         # Return the list of grouped anagrams
         return list(anagrams.values())
 
-# Example usage:
 sol = Solution()
 print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
